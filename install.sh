27c98300329b3a08fdcbadf74bac833755b04f06
therealmaass <ominousvomit@googlemail.com>
2020-11-26 08:28:13 +0100

    Fixes on install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 #Define standard tools to be installed:
-tools="zsh vim tmux ranger stow wget curl"
+tools="zsh vim tmux ranger stow wget curl git"
+toolsprint="# ${tools}     #"
 cd ~
 which $tools > /dev/null 2>&1
 
@@ -10,44 +11,52 @@ if [ $? != 0 ]; then
         OS=$(cat /etc/os-release | grep "ID_LIKE" | cut -d= -f2)
         echo $OS
         if [ "$OS" = "debian" ]|| [ "$OS" = "ubuntu" ]; then
-            echo "################################"
-            echo "# Installing standard tools:   #"
-            echo "################################"
+            echo "##############################################"
+            echo "# Installing standard tools:                 #"
+            echo "$toolsprint"
+            echo "##############################################"
             sudo apt-get update && sudo apt-get install -y $tools 
             sleep 3
-            echo "################################"
-            echo "# Install standard tools done! #"
-            echo "################################"
+            echo "##############################################"
+            echo "# Install standard tools done!               #"
+            echo "##############################################"
             sleep 3
-            echo "################################"
-            echo "# Installing Oh-my-zsh & P10K  #"
-            echo "################################"
+            echo "##############################################"
+            echo "# Installing Oh-my-zsh & P10K                #"
+            echo "##############################################"
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-            echo "#################################"
-            echo "# Install Oh-my-zsh & P10K done!##"
-            echo "#################################"
+            curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+            echo "###############################################"
+            echo "# Install Oh-my-zsh & P10K done!             #"
+            echo "###############################################"
         elif [ "$OS" = "arch" ]; then
             echo "THIS IS ARCH"
-            echo "################################"
-            echo "# Installing standard tools:   #"
-            echo "################################"
+            echo "##############################################"
+            echo "# Installing standard tools:                 #"
+            echo "# $(tools)                                   #"
+            echo "##############################################"
             sleep 3
-            echo "################################"
+            echo "##############################################"
             sudo pacman -Syy && sudo pacman -S --noconfirm $tools
-            echo "################################"
-            echo "# Install standard tools done! #"
-            echo "################################"
+            echo "##############################################"
+            echo "# Install standard tools done!               #"
+            echo "##############################################"
             sleep 3
-            echo "################################"
-            echo "# Installing Oh-my-zsh & P10K  #"
-            echo "################################"
+            echo "##############################################"
+            echo "# Installing Oh-my-zsh & P10K                #"
+            echo "##############################################"
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-            echo "#################################"
-            echo "# Install Oh-my-zsh & P10K done!##"
-            echo "#################################"
+            curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+            echo "###############################################"
+            echo "# Install Oh-my-zsh & P10K done!             #"
+            echo "###############################################"
         else
             echo "JAAA"
         fi
     fi
+else
+    echo "###############################################"
+    echo "# Nothing to do all tools are installed! :    #"
+    echo "###############################################"
+    which $tools
 fi
