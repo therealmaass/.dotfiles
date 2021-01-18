40576c09824126f1bd4fc13fd48ee4e044990dc1
therealmaass <ominousvomit@googlemail.com>
2021-01-18 18:38:18 +0100

    Added zsh plugins to install script

--- install.sh
+++ install.sh
@@ -27,6 +27,9 @@ if [ $? != 0 ]; then
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
             curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
             git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+            #Install zsh-syntax-highlighting and zsh-autosuggestions
+            git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+            git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
             echo "###############################################"
             echo "# Install Oh-my-zsh & P10K done!             #"
             echo "###############################################"
@@ -49,6 +52,9 @@ if [ $? != 0 ]; then
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
             curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
             git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+            #Install zsh-syntax-highlighting and zsh-autosuggestions
+            git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+            git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
             echo "###############################################"
             echo "# Install Oh-my-zsh & P10K done!             #"
             echo "###############################################"
